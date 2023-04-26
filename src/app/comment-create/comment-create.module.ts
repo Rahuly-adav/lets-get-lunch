23f8a052b5be577e22c6f39528ed599a84329aaf
Rahul <P3184538>
2023-04-26 10:27:36 +0530

    Added Comment Sevice

---
+++ src/app/comment-create/comment-create.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CommentCreateComponent } from './comment-create.component';
+
+
+
+@NgModule({
+  declarations: [
+    CommentCreateComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CommentCreateModule { }
